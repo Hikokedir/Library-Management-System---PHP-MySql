df84d867a31c59d5b683c26d1c10f58fbd5200cf
Anko <69928310+Hikokedir@users.noreply.github.com>
2021-07-24 21:09:14 +0300

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2018 Online Library Management System |<a href="https://www.linkedin.com/in/aleksandart96/" target="_blank"  > Designed by : Aleksandar Trifunovic</a> 
+                   &copy; 2021 RVU Online Library Management System |<a href="https://www.linkedin.com/in/aleksandart96/" target="_blank"  > Designed by : Hiko Kedir</a> 
                 </div>
 
             </div>
